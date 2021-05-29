e3819376b0006f70fb927b4a0a8b6b476a518995
musti <mustafakaratas4179@gmail.com>
2021-05-30 00:01:26 +0300

    master2

--- client/src/App.js
+++ client/src/App.js
@@ -180,7 +180,7 @@ class App extends Component {
         }
         else {
           message = "Item " + product.name + " was shipped to the customer!"
-        }//
+        }//a
 
         // Displaying notification that the product was paid
         self.setState({
